a0d9969358d5309dcaf9a53034233bc776dc241c
StoneWolfCJQ <StoneWolfCJQ@gmail.com>
2020-03-31 17:27:18 +0800

    Initial commit

---
+++ CrackRenamer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackRenamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length > 0)
+                {
+                    System.IO.File.Move(args[0], args[1]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ EnCrack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace EnCrack
+{
+    //Change the program assembly name on ur encryption software's whitelist
+    //For example, "Acrobat.exe"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Crack();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        static void Crack()
+        {
+            string inFileName;
+            inFileName = Console.ReadLine().Trim('\"');
+            byte[] fb = File.ReadAllBytes(inFileName);
+            string outFileName = Path.GetDirectoryName(inFileName) + 
+                '\\'+Path.GetFileNameWithoutExtension(inFileName);
+            File.WriteAllBytes(outFileName, fb);
+
+            ProcessStartInfo ps = new ProcessStartInfo("CrackRenamer.exe");
+            ps.CreateNoWindow = true;
+            ps.UseShellExecute = false;
+            ps.Arguments = '\"' + outFileName + '\"' + " " 
+                + '\"' + outFileName + "-Copy" + Path.GetExtension(inFileName) + '\"';
+
+            Process.Start(ps).WaitForExit();
+        }
+    }
+}
